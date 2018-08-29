bf33e04e590adeda5ccd03135c3a4c2136e3d8c9
Yash N. Shah <yshah@linkedin.com>
2018-08-29 07:45:20 +0200

    fixing ip2coordinates.py with error handling

--- ip2coordinates.py
+++ ip2coordinates.py
@@ -3,5 +3,9 @@ from geolite2 import geolite2
 with open('/tmp/ip.txt') as f:
     reader = geolite2.reader()
     for line in f:
-        print(reader.get(line.strip()).get('location').get('longitude'), end=' '),
-        print(reader.get(line.strip()).get('location').get('latitude'))
+        try:
+            print(reader.get(line.strip()).get('location').get('longitude'), end=' '),
+            print(reader.get(line.strip()).get('location').get('latitude'))
+        except Exception as  e:
+            pass
+
